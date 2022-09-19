c802e754509a5b107ce692bbc0114047df2720f4
JuanJo2804 <2804juanjo@gmail.com>
2022-09-19 13:06:48 -0500

    Co-authored-by: Kolozuz <Kolozuz@users.noreply.github.com>

--- Models/Curso.php
+++ Models/Curso.php
@@ -1,8 +1,18 @@
 <?php
     include '../../Config/Conexion.php';
+
     $conexion = new Conexion();
-    $sql = "SELECT * FROM cursos WHERE id_curso = 1";
+    $sql = "SELECT * FROM cursos";
+
     $read = $conexion->stm->prepare($sql);
     $read->execute();
+    
     $cursoobjeto = $read->fetchAll(PDO::FETCH_OBJ);
+
+    $htmlsql = "SELECT * FROM cursos WHERE ";
+
+    $htmlread = $conexion->stm->prepare($htmlsql);
+    $htmlread->execute();
+    
+    $htmlobj = $htmlread->fetchAll(PDO::FETCH_OBJ);
  ?>
