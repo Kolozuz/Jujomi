15db2b209878b8f2223c57c3d3f027cee4f579c6
Kolozuz <moralesjuanpablo1122@gmail.com>
2022-09-27 15:58:49 -0500

    minimun changes

---
+++ Views/Usuario/FaqUsuario.php
@@ -0,0 +1,14 @@
+<?php include '../Inc/userheader.php'; ?>
+
+    <div class="container-fluid p-5 text-center">
+        <div class="row m-5">
+            <div class="col">
+                <h2>Bienvenido a la seccion de Preguntas Frecuentes</h2> 
+            </div>
+        </div>
+        <div class="row fs-5">
+            
+        </div>
+    </div>
+
+<?php include '../Inc/userfooter.php' ?>
