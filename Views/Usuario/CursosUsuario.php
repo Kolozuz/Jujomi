bff762e60dab1ccc01ed0ca4e071c53819d84743
Kolozuz <moralesjuanpablo1122@gmail.com>
2023-01-23 23:49:24 -0500

    deleted cuz wasn't practical

--- Views/Usuario/CursosUsuario.php
+++
@@ -1,7 +0,0 @@
-<?php include '../Inc/userheader.php'; ?>
-
-<?php include '../Views/Usuario/CursosManager.php'; ?>
-
-<?php include '../Views/Usuario/CursosViewer.php'; ?>
-
-<?php include '../Inc/userfooter.php' ?>
